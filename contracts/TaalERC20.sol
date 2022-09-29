67abab8bf515c06d8a80a6bc1ed4fde460af732c
Peter H. Namh <hwnahm@gmail.com>
2022-09-29 11:51:23 +0900

    support Klaytn Cypress

--- contracts/TaalERC20.sol
+++ contracts/TaalERC20.sol
@@ -84,7 +84,7 @@ contract TaalERC20 is ITaalERC20 {
 
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
         require(deadline >= block.timestamp, 'Taal: EXPIRED');
-        uint chainId = 1001;
+        uint chainId = 8217;
 //        assembly {
 //            chainId := chainid
 //        }
